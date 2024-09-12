4f4c9b6cf0711e75aeb4ae3bf0883ab27c3d7864
Bharat Pahwa <pahwabharat15@gmail.com>
2024-09-12 11:07:43 +0530

    Add/Analytics

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './components/Auth/Login';
 import SignUp from './components/Auth/Signup';
 import SellerInfo from './components/UserInfo/SellerInfo';
 import BuyerInfo from './components/UserInfo/BuyerInfo';
+import { Analytics } from '@vercel/analytics/react';
 import { Routes, Route } from 'react-router-dom';
 import ProductList from './components/Products/Listing';
 import Order from './components/Orders/Order';
@@ -15,6 +16,7 @@ function App() {
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<SignUp />} />
       {/* <Route path='/buyerinfo' element={<BuyerInfo />} /> */}
+      <Analytics />
       <Route path='/sellerinfo' element={<SellerInfo />} />
       <Route path='/products' element={<ProductList />} />
       {/* <Route path='/orders' element={<Order />} /> */}
