37621d5060d07cab61d56365820ab69dad6f8b9c
Bharat Pahwa <pahwabharat15@gmail.com>
2024-09-13 20:52:16 +0530

    Buy Live

--- src/components/Alerts/Success.js
+++ src/components/Alerts/Success.js
@@ -12,7 +12,7 @@ const SuccessAlert = ({ message, onClose }) => {
   return (
     <div className='fixed top-0 left-0 right-0 z-50 w-max m-auto items-center flex justify-center'>
       <div
-        className='bg-green-500 text-white py-2 px-4 rounded-md shadow-lg mt-8 transform transition-all duration-500'
+        className='bg-green-100 text-black py-2 px-4 rounded-md shadow-lg mt-8 transform transition-all duration-500'
         style={{ opacity: message ? 1 : 0, translateY: message ? 0 : '-100%' }}
       >
         <p>{message}</p>
--- src/components/HeroSection/Hero.js
+++ src/components/HeroSection/Hero.js
@@ -24,13 +24,13 @@ export default function Hero() {
             A platform where seniors can sell their drafters to juniors
           </div>
           <div className='flex w-full lg:md:justify-start gap-5 justify-center self-start mt-8 text-base leading-7 capitalize whitespace-nowrap'>
-            {/* <NavLink
+            <NavLink
               to='/signup'
               onClick={() => handleOptionClick('BUY')}
-              className='flex flex-col justify-center px-11 py-2.5 not-italic text-white bg-gradient-to-r from-slate-900 to-slate-700 rounded-[150px] max-md:px-10'
+              className='flex flex-col justify-center px-11 py-2.5 not-italic text-black bg-white border-black border-2 rounded-[150px] max-md:px-10'
             >
               BUY
-            </NavLink> */}
+            </NavLink>
             <div className='flex flex-col justify-center '>
               <NavLink
                 to='/signup'
@@ -57,12 +57,12 @@ export default function Hero() {
           </div>
           <div className='flex gap-5 w-full self-start mt-16 text-black max-md:mt-10'>
             <div className='flex flex-col w-full items-start gap-3 my-auto'>
-              {/* <div className='text-[1.2rem] text-center lg:md:text-left w-full not-italic'>
+              <div className='text-[1.2rem] text-center lg:md:text-left w-full not-italic'>
                 Our Average Price - ₹350{' '}
               </div>
               <div className='text-[1.2rem] text-center lg:md:text-left w-full not-italic'>
                 Market Average Price - ₹650{' '}
-              </div> */}
+              </div>
             </div>
           </div>
         </div>
